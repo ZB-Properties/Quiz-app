e1ac160d733cfe94c9d289e6dff45ff3939b9063
Shope Alli-B <zoeyalli921@gmail.com>
2025-04-09 16:51:12 -0700

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question: "Which of the following is an example of an open-source operating system?",
+        option1: "Windows 11",
+        option2: "MacOS",
+        option3: "Linux",
+        option4: "iOS",
+        ans: 3,
+    },
+    {
+        question: "What is the primary purpose of Artificial Intelligence (AI)?",
+        option1: "To create machines that can replicate human intelligence",
+        option2: "To design new software applications",
+        option3: "To store data more efficiently",
+        option4: "To provide internet security",
+        ans: 1,
+    },
+    {
+        question: "Which of these technologies is most commonly associated with cryptocurrencies?",
+        option1: "Blockchain",
+        option2: "Virtual Reality",
+        option3: "5G",
+        option4: "Cloud computing",
+        ans: 1,
+    }, 
+    {
+        question: "What is the primary function of an operating system (OS)?",
+        option1: "To run applications",
+        option2: "To manage hardware and software resources",
+        option3: "To access the internet",
+        option4: "To create documents",
+        ans: 2,
+    },  
+    {
+        question: "Which programming language is most commonly used for web development?",
+        option1: "Java",
+        option2: "C++",
+        option3: "Javascript",
+        option4: "Python",
+        ans: 3,
+    }, 
+
+]
